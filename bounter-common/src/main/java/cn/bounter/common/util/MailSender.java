844730498c96899e6da6e14ce61094b0ffc95ec9
Simon-MiniPC\Simon <1317492210@qq.com>
2018-11-06 18:25:31 +0800

    bounter-common 3.3:增加邮件发送组件

---
+++ bounter-common/src/main/java/cn/bounter/common/util/MailSender.java
@@ -0,0 +1,96 @@
+package cn.bounter.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 邮件发送工具,需要子类继承使用
+ * @author simon
+ *
+ */
+public class MailSender {
+
+	private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
+
+	@Autowired
+    private JavaMailSender javaMailSender;
+
+
+	/**
+	 * 群体发送简单的文本邮件
+	 * @param sendFrom
+	 * @param sendTo
+	 * @param subject
+	 * @param textcontent
+	 */
+	public void sendSimpleMessage(String sendFrom, String[] sendTo, String subject, String textcontent) {
+		try {
+			SimpleMailMessage mail = new SimpleMailMessage();
+			mail.setFrom(sendFrom);
+			mail.setTo(sendTo);
+			mail.setSubject(subject);
+			mail.setText(textcontent);
+			//发送
+			javaMailSender.send(mail);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+
+	/**
+	 * 单个发送简单文本邮件
+	 * @param sendFrom
+	 * @param sendTo
+	 * @param subject
+	 * @param textcontent
+	 */
+	public void sendSimpleMessage(String sendFrom, String sendTo, String subject, String textcontent) {
+		sendSimpleMessage(sendFrom, new String[]{sendTo}, subject, textcontent);
+	}
+
+
+	/**
+	 * 群体发送HTML内容格式的邮件
+	 * @param sendFrom
+	 * @param sendTo			收件人组
+	 * @param subject			主题
+	 * @param htmlContent		HTML内容
+	 */
+	public void sendHtmlMessage(String sendFrom, String[] sendTo, String subject, String htmlContent) {
+		try {
+			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+			mimeMessageHelper.setFrom(sendFrom);
+			mimeMessageHelper.setTo(sendTo);
+			mimeMessageHelper.setSubject(subject);
+			// true 表示启动HTML格式的邮件
+			mimeMessageHelper.setText(htmlContent, true);
+
+			// 发送邮件
+			javaMailSender.send(mimeMessage);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+
+	/**
+	 * 单个发送HTML内容格式的邮件
+	 * @param sendFrom
+	 * @param sendTo			单个收件人
+	 * @param subject			主题
+	 * @param htmlContent		HTML内容
+	 * @throws Exception
+	 */
+	public void sendHtmlMessage(String sendFrom, String sendTo, String subject, String htmlContent) {
+		sendHtmlMessage(sendFrom, new String[]{sendTo}, subject, htmlContent);
+	}
+}
