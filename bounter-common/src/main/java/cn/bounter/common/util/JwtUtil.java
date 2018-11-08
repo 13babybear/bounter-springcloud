ee96cf3424596c9e16e9595e510e307687b4b7f9
Simon-MiniPC\Simon <1317492210@qq.com>
2018-11-08 18:18:16 +0800

    bounter-common 3.5:增加JWT工具

---
+++ bounter-common/src/main/java/cn/bounter/common/util/JwtUtil.java
@@ -0,0 +1,53 @@
+package cn.bounter.common.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+public class JwtUtil {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+
+	/** JWT签名密钥 */
+    private static final String JWT_SECRET_KEY = "U2ltb25Mb3ZlU3VzYW4=";
+    /** accessToken失效时间，单位'分钟' */
+    private static final Integer ACCESS_TOKEN_EXPIRE_TIME = 30;
+
+    /**
+     * 生成accessToken
+     * @param userId
+     * @return
+     */
+    public static String generateAccessToken(String userId) {
+        JwtBuilder builder = Jwts.builder()
+                .setSubject(userId)
+                .setExpiration(DateUtil.getDateMinutesLater(ACCESS_TOKEN_EXPIRE_TIME))		//30分钟后过期
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY);
+        return builder.compact();
+    }
+
+
+    /**
+     * 解析JWT，并获取用户ID
+     * @param jwt
+     * @return
+     */
+    public static String parseToken(String jwt){
+        if (StringUtils.isEmpty(jwt)) {
+            return null;
+        }
+
+        try {
+            Claims claims = Jwts.parser().setSigningKey(JWT_SECRET_KEY).parseClaimsJws(jwt).getBody();
+            //获取用户身份信息
+            return claims.getSubject();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+    }
+    
+}
